77f60ac53f08bf268e22ddfd29b18f1f2d8fc97d
Berecocheaidoia1 <135612592+Berecocheaidoia1@users.noreply.github.com>
2023-06-07 08:50:40 -0300

    ejercicio-1-node.js

---
+++ eje2.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+var valor1= parseInt(prompt("ingrese un numero: "));
+var valor2= parseInt(prompt("ingrese un numero: "));
+
+var sumar = valor1+valor2;
+
+console.log("el primer valor es: "+valor1+" el segundo valor es"+valor2 +" yla suma de numeros es:" +sumar);
+//
